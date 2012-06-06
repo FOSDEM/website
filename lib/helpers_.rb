909ce0389fbc97cab4cfb94aa31ae06bc9b25bab
Pascal Bleser <pascal.bleser@opensuse.org>
2012-06-06 06:58:10 +0200

    move out the monkey patching on Nanoc::Item into its own file

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -1,48 +1,7 @@
 # vim: set ts=2 sw=2 et ai ft=ruby:
-#
+
 include NavTreeHelper
 include BCrumbs
 include Nanoc::Helpers::LinkTo
-include Nanoc::Helpers::Breadcrumbs
 include Nanoc::Helpers::Rendering
-
-class Nanoc::Item
-  def pathparts
-    p.split(%r{/+})
-  end
-  def navtitle
-    if self[:navtitle] then
-      self[:navtitle]
-    elsif self[:title] then
-      self[:title]
-    else
-      self.id.capitalize
-    end
-  end
-  def interview?
-    self[:kind] == 'interview' or self.identifier =~ %r{^/interviews?/.+}
-  end
-  def page?
-    self[:kind] == 'page' or self.identifier.match(%r{^/(news|headlines|assets|sponsors?)/}).nil?
-  end
-  def news?
-    self[:kind] == 'news' or self.identifier.match(%r{^/news/.+}).nil? ? false : true
-  end
-  def sponsor?
-    self[:kind] == 'sponsor' or (self[:kind] != 'page' and not self.binary? and self.identifier.match(%r{^/sponsors?/.+/}).nil? ? false : true)
-  end
-  def disabled?
-    self[:disabled]
-  end
-  def enabled?
-    not self.disabled?
-  end
-  def id
-    self.identifier.gsub(%r{/+$}, '').split('/')[-1]
-  end
-  def p
-    self.identifier.gsub(%r{^/}, '').gsub(%r{/$}, '')
-  end
-end
-
 
