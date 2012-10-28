90334b1e6e0e3cd330315e322f03b6212a137b1f
Pascal Bleser <pascal.bleser@opensuse.org>
2012-10-28 02:09:27 +0200

    add Solr indexing support

---
+++ commands/index.rb
@@ -0,0 +1,172 @@
+# vim: set ts=2 sw=2 et ai ft=ruby:
+
+usage       'index'
+summary     'loads the content (in output/) into the Solr search index'
+aliases     :i, :solr
+flag        :l, :live, "operate on a live Solr instance"
+option      :o, :output, "output file for the Solr add XML document", :argument => :required
+
+class SolrIndex < ::Nanoc::CLI::CommandRunner
+  def run
+    total_time = start_time = Time.now
+    if options[:live]
+      require 'rsolr'
+    end
+    require 'builder'
+    require 'nokogiri'
+    #require 'hpricot_scrub'
+    require 'time'
+    self.require_site
+
+    self.site.compiler.load
+
+    solr = begin
+             if options[:live]
+               conf = site.config.fetch :solr, {}
+               url = conf.fetch 'url', 'http://localhost:8983/solr/'
+               RSolr.connect :url => url
+             else
+               nil
+             end
+           end
+
+    file_prefix = site.config.fetch 'output_dir', 'output'
+
+    items = self.site.items.reject do |item|
+      item.reps.empty? or
+      item.binary? or
+      item.reps.empty? or
+      item.reps.first.raw_paths.empty? or
+      item.reps.first.path.nil?
+    end
+    indexable_items = items.map do |item|
+      file = File.join file_prefix, item.reps.first.path
+      file << "index.html" if file.end_with? '/'
+      { id: item.identifier, file: file, item: item }
+    end.reject{|item| item[:item][:kind] == 'internal'}
+    .select{|item| item[:file] =~ %r{\.html$}}
+
+    xml = Builder::XmlMarkup.new
+    docs = []
+    begin
+      xml.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
+      xml.add do
+        indexable_items.each do |i|
+          start = Time.now
+          begin
+            item = i[:item]
+            types = case item.identifier
+                    when %r{^/schedule/room}
+                      [:schedule, :room]
+                    when %r{^/schedule/track}
+                      [:schedule, :track]
+                    when %r{^/schedule/speaker}
+                      [:schedule, :speaker]
+                    when %r{^/schedule/event}
+                      [:schedule, :event]
+                    when %r{^/schedule/day}
+                      [:schedule, :day]
+                    when %r{^/schedule/}
+                      [:schedule]
+                    when %r{^/news/}
+                      [:news]
+                    when %r{^/interview/}
+                      [:interview]
+                    else
+                      [:content]
+                    end
+            #content = Nokogiri::HTML(item.raw_content).text
+            raise "item #{item.identifier} has #{item.reps.size}, can't decide which one to use" unless item.reps.size == 1
+            #raw_content = item.compiled_content(:snapshot => :pre)
+            raw_content = item.reps.first.content[:last]
+            content = Nokogiri::HTML(item.reps.first.content[:last]).text.strip
+            #content = Hpricot(raw_content).scrub.to_s.strip
+
+            docs << {
+              :id => item.path,
+              :title => item[:title],
+              :type => types.map(&:to_s),
+              :content => content,
+            }
+
+            xml.doc do
+              xml.field(item.path, :name => "id")
+              if item[:title]
+                xml.field(item[:title], :name => "title")
+              else
+                $stderr.puts "HUH, no title for #{item.path} ?"
+              end
+              types.map(&:to_s).each do |t|
+                xml.field(t, :name => 'type')
+              end
+              if content and not content.empty?
+                xml.field(content, :name => 'content')
+              end
+            end
+          rescue
+            $stderr.puts "failed to index #{item.identifier}"
+            raise
+          end
+        end
+      end
+    end
+
+    if solr
+      begin
+        start = Time.now
+        solr.delete_by_query '*:*'
+        log :low, :delete, '*:*', Time.now - start
+      end
+
+      begin
+        start = Time.now
+        begin
+          solr.add docs
+        rescue RSolr::Error::Http => e
+          body = eval e.response[:body]
+          raise "Solr responded with HTTP code #{e.response[:status]}: #{body['error']['msg']}"
+        end
+        log :low, :index, "#{docs.size} documents out of #{self.site.items.size} items", Time.now - start
+      end
+
+      [ :commit, :optimize ].each do |action|
+        start = Time.now
+        solr.method(action).call
+        log :low, action, '', Time.now - start
+      end
+    end
+
+    if options[:output]
+      start = Time.now
+      File.open(options[:output], "w") do |f|
+        f.puts xml.target!
+      end
+      log :high, :write, "Solr <add/> written to #{options[:output]}", Time.now - total_time
+    end
+
+  end
+
+  private
+  ACTION_COLORS = {
+    :delete   => "\e[1;31m",
+    :index    => "\e[1;32m",
+    :write    => "\e[1;32m",
+    :commit   => "\e[1;33m",
+    :optimize => "\e[1;33m",
+  }
+  def log(level, action, name, duration=nil)
+    Nanoc::CLI::Logger.instance.log(
+      level,
+      '%s%12s%s  %s%s' % [
+        ACTION_COLORS[action.to_sym],
+        action.to_s,
+        "\e[0m",
+        duration.nil? ? '' : "[%2.2fs]  " % [ duration ],
+        name
+      ]
+    )
+  end
+end
+
+runner SolrIndex
+
---
+++ commands/query.rb
@@ -0,0 +1,86 @@
+# vim: set ts=2 sw=2 et ai ft=ruby:
+
+usage       'query [query]'
+summary     'queries Solr'
+aliases     :q
+flag        :f, :facets,    'do a faceted search'
+
+class SolrQuery < ::Nanoc::CLI::CommandRunner
+  def run
+    raise Nanoc::Errors::GenericTrivial, "usage: #{command.usage}" if arguments.size != 1
+    require 'rsolr'
+    self.require_site
+    solr = begin
+             conf = site.config.fetch :solr, {}
+             url = conf.fetch 'url', 'http://localhost:8983/solr/'
+             RSolr.connect :url => url
+           end
+
+    params = begin
+               p = {
+                 :wt => :ruby,
+                 :q => arguments[0],
+                 :fl => '*,score',
+                 :hl => 'true',
+                 'hl.fl' => 'title',
+                 'hl.simple.pre' => '<HL>',
+                 'hl.simple.post' => '</HL>',
+                 'hl.requireFieldMatch' => 'false',
+                 'hl.usePhraseHighlighter' => 'false',
+               }
+               if options[:facets]
+                 p[:facet] = 'true'
+                 p['facet.field'] = 'type'
+               end
+               p
+             end
+
+    response = solr.get 'select', :params => p
+    puts "\e[32;1mFound #{response['response']['numFound']} result(s)\e[0m"
+    puts
+    max_score = response['response']['maxScore'].to_f
+
+    if response.has_key? 'facet_counts'
+      puts "\e[36;1;4mFacets:\e[0m"
+      Hash[*(response['facet_counts']['facet_fields']['type'])]
+      .reject{|facet, count| count < 1}
+      .sort_by{|facet, count| count}
+      .reverse
+      .each do |facet, count|
+        puts "\e[30;1m*\e[0m #{facet} (#{count})"
+      end
+      puts
+    end
+    hl = response.fetch 'highlighting', {}
+    begin
+      puts "\e[36;1;4mResults:\e[0m"
+      response['response']['docs'].each do |doc|
+        stars = begin
+                  rating = (((doc['score'].to_f / max_score) * 100) + 0.5).to_i / 20
+                  "\e[33;1m" + ("\u{2605}" * rating) + "\e[0;37m" + ("\u{2606}" * (5 - rating)) + "\e[0m"
+                end
+        puts "#{stars} \e[35;4;1m#{doc['id']}\e[0m"
+        title = begin
+                  h = hl[doc['id']]
+                  if h
+                    t = h['title']
+                    unless t.nil? or t.empty?
+                      t.first.gsub('<HL>', "\e[43;30;1m").gsub('</HL>', "\e[0m")
+                    else
+                      doc['title']
+                    end
+                  else
+                    doc['title']
+                  end
+                end
+
+        puts "      #{title}"
+        puts "      \e[32m#{doc['type'].join(', ')}\e[0m" if doc['type']
+        puts
+      end
+    end
+  end
+end
+
+runner SolrQuery
+
