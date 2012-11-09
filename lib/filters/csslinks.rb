da0aba12f8e55da1092df46ca458b9dd1d11b65e
Pascal Bleser <pascal.bleser@opensuse.org>
2012-11-09 22:24:05 +0100

    add nanoc filter for adapting url(...) links to output path in css files

---
+++ lib/filters/csslinks.rb
@@ -0,0 +1,36 @@
+# vim: set ts=2 sw=2 et ai ft=ruby fileencoding:utf-8:
+
+module Fosdem
+  class CssLinks < Nanoc::Filter
+    type :text
+    identifier :csslinks
+
+    require 'nanoc/helpers/link_to'
+    include Nanoc::Helpers::LinkTo
+
+    def run(content, params={})
+      @item_rep = assigns[:item_rep] if @item_rep.nil?
+
+      content.gsub(%r{url\((.+?)\)}) do
+        f = $1
+        b, e = begin
+              r = f.rindex '.'
+              if r
+                [ f[0..r-1], f[r+1..-1] ]
+              else
+                [ f, '' ]
+              end
+            end
+
+        id = '/' + @item.identifier[1..-2].split('/')[0..-2].join('/') + '/' + b + '/'
+        target = $item_by_id.fetch(id) do
+          fail "#{@item.identifier}: references \"#{f}\" but failed to find matching identifier \"#{id}\""
+        end
+        raise "#{@item.identifier}: references \"#{f}\" and the matching identifier \"#{id}\" exists, but the extension does not match: item has #{target[:extension]}" unless target[:extension] == e
+
+        "url(#{target.path})"
+      end
+
+    end
+  end
+end
