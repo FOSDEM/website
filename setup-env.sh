8ea9c37280b1e117e9853caf2ff1d908a550f038
Philip Paeps <philip@fosdem.org>
2012-11-20 03:57:03 +0100

    setup-env: fail more gracefully and offer advice

--- setup-env.sh
+++ setup-env.sh
@@ -1,8 +1,13 @@
 # source.  don't exec.
 
 if [[ ! -s $HOME/.rvm/scripts/rvm ]]; then
-	echo "\033[31;4;1mError: You should probably install RVM.\033[0m"
-	exit 42
+	echo "\033[31;4;1mError: rvm not installed\033[0m"
+	echo "Read the instructions at https://rvm.io/rvm/install/."
+	echo
+	echo "Note well: the rvm installation will modify your shellrc"
+	echo "files in a trojan-esque way.  Carefully audit your .z* or"
+	echo "your .bash* and .profile files after installation!"
+	return
 fi
 
 source $HOME/.rvm/scripts/rvm
