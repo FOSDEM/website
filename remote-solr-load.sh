4bbcd999a1168a7757f7844101f1e03c3a89b7c7
Gerry Demaret <gerry@fosdem.org>
2019-04-23 19:39:40 +0200

    search: remove obsolete solr scripts

--- remote-solr-load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-tty >/dev/null && {
-    echo "ERROR: you must pipe documents into this script" >&2
-    exit 1
-}
-
-exec ssh search@nanoc.fosdem.org "/home/search/website/solr-load.sh"
